74fdbb050e25b1eae139145533c87c3783f0def3
youtsuho <110821381+youtsuhodev@users.noreply.github.com>
2026-07-23 17:29:53 +0200

    chore: commit all pending changes

--- src/youcordplugins/youcordRPC/index.tsx
+++ src/youcordplugins/youcordRPC/index.tsx
@@ -2,7 +2,6 @@ import definePlugin from "@utils/types";
 import { FluxDispatcher } from "@webpack/common";
 
 const APP_ID = "1529869867640029386";
-const IMAGE_URL = "https://i.ibb.co/6cbzxqsn/68747470733a2f2f692e6962622e636f2f52344857637059482f436861742d4750542d496d6167652d31322d6a75696c2d32.png";
 const SOCKET_ID = "YouCordRPC";
 
 function setActivity() {
@@ -14,10 +13,6 @@ function setActivity() {
             name: "YouCord",
             details: "Modding Discord",
             state: "Injected ✓",
-            assets: {
-                large_image: `mp:external/youcord/${encodeURIComponent(IMAGE_URL)}`,
-                large_text: "YouCord"
-            },
             timestamps: {
                 start: Date.now()
             },
